d80c9e3ee21afe14686f4268c65d5a601e1bda62
Hemil78 <zanattahemil@gmail.com>
2021-10-05 18:32:17 +0200

    layout base

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,48 @@
 <template>
   <header>
-      
+    <div class="box-logo">
+      <h1>BOOLFLIX</h1>
+    </div>
+    <div class="box-search">
+      <input type="search">
+    </div>
+
   </header>
 </template>
 
 <script>
 export default {
     name: "Header",
-    props: {
-      
-    }
-    
+    props: ['logo']
 }
 </script>
 
 <style lang="scss" scoped>
 
 @import '../assets/common.scss';
+
+header{
+  background-color: #000;
+  padding: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+
+  .box-logo{
+
+    h1{
+      color: red;
+    }
+  }
+  .box-search{
+
+    input{
+      padding: 8px 12px;
+      border-radius: 5px;
+    }
+  }
+}
+
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,39 @@
 <template>
   <main>
-      <Card/>
+    <div class="box-cards">
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <div class="box-card">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ipsum. Sit sapiente numquam, voluptates reprehenderit, quia ullam adipisci, illo possimus fugit repudiandae a explicabo ab vel vitae minus cumque incidunt!
+            
+        </div>
+        <Card/>
+    </div>
+      
   </main>
 </template>
 
 <script>
 
-import axios from 'axios';
+
 import Card from './Card.vue';
 
 export default {
@@ -22,5 +49,22 @@ export default {
 <style lang="scss" scoped>
 
 @import '../assets/common.scss';
+
+main{
+    background-color: $ColorSmallText;
+    padding: 20px 0;
+
+    .box-cards{
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+
+        .box-card{
+            width: calc(100% / 5 - 40px);
+            margin: 0 20px;
+        }
+    }
+}
 
 </style>
